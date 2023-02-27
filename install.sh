ed09cfad2cef966dec3774cc44e633ca9f1b0737
Naerhy <naerhy@protonmail.com>
2023-02-28 00:42:53 +0100

    Refactor code outside functions (install.sh)

--- install.sh
+++ install.sh
@@ -4,27 +4,45 @@ RED="\033[0;31m"
 GREEN="\033[0;32m"
 NO_COLOR="\033[0m"
 
-if [[ $EUID -ne 0 || ! -f "/etc/debian_version" ]]; then
-	echo -e "${RED}Error${NO_COLOR}: no root privileges or no Debian-system"
-	exit 1
-fi
+check_system () {
+	if [[ $EUID -ne 0 || ! -f "/etc/debian_version" ]]; then
+		echo -e "${RED}Error${NO_COLOR}: no root privileges or no Debian-system"
+		exit 1
+	fi
+}
 
-is_vim_installed=$(dpkg -s vim | grep "Status: install ok installed")
-if [[ -z $is_vim_installed ]]; then
-	apt update && apt install -y vim
-fi
-rm -f ~/.vimrc
-rm -rf ~/.vim
+install_requirements () {
+	local vim=$(dpkg -s vim | grep "Status: install ok installed")
+	local curl=$(dpkg -s curl | grep "Status: install ok installed")
+	if [[ -z $vim ]]; then
+		apt update && apt install -y vim
+	fi
+	if [[ -z $curl ]]; then
+		apt update && apt install -y curl
+	fi
+}
 
-cp .vimrc ~
-cp -r .vim ~
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-vim -c PlugInstall -c qa
+configure_vim () {
+	local url="https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
+	rm -f ~/.vimrc
+	rm -rf ~/.vim
+	cp .vimrc ~
+	cp -r .vim ~
+	curl -fLo ~/.vim/autoload/plug.vim --create-dirs $url
+	vim -c PlugInstall -c qa
+}
 
-is_font_installed=$(fc-list | grep JetBrainsMonoNL-Regular.ttf)
-if [ -z "$is_font_installed" ]; then
-	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/JetBrains/JetBrainsMono/master/install_manual.sh)"
-fi
+install_fonts () {
+	local font=$(fc-list | grep JetBrainsMonoNL-Regular.ttf)
+	if [[ -z $font ]]; then
+		local url="https://raw.githubusercontent.com/JetBrains/JetBrainsMono/master/install_manual.sh"
+		/bin/bash -c "$(curl -fsSL $url)"
+	fi
+}
 
+check_system
+install_requirements
+configure_vim
+install_fonts
 echo -e "Installation has been ${GREEN}completed${NO_COLOR}"
 exit 0
